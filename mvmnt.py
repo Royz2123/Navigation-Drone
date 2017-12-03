fe5a42f484e624fb03078bbf2bbec1e18267977c
Royz2123 <royzohar25@gmail.com>
2017-12-03 19:21:57 +0200

    Added video mode - works. fixed things with the video encoder. Added video with pixel shifts

--- mvmnt.py
+++ mvmnt.py
@@ -19,7 +19,7 @@ def print_usage():
                     IM_MODE:     [im1]     [im2]
     """
 
-def print_transform(im1, im2):
+def do_transform(im1, im2):
     # normalize
     im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2HSV)
     im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2HSV)
@@ -30,7 +30,24 @@ def print_transform(im1, im2):
         im2,
         fullAffine=True
     )
-    print(transform)
+
+    if transform is None:
+        raise RuntimeError("Couldn't find the transform")
+
+    return transform[0][2], transform[1][2]
+
+
+def add_text(im, txt, pos):
+    cv2.putText(
+        im,
+        txt,
+        pos,
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.8,
+        (0, 0, 255),
+        2,
+        cv2.LINE_AA
+    )
 
 
 def main():
@@ -48,38 +65,75 @@ def main():
         im2 = cv2.imread(sys.argv[3])
 
         # compute transform
-        print_transform(im1, im2)
+        do_transform(im1, im2)
 
     else:
         # define the two images
         new_im = None
         old_im = None
 
-        print sys.argv[2]
+        # define the current position
+        curr_pos = [0, 0, 50]
 
-        cap = cv2.VideoCapture(sys.argv[2])
+        # try to see if this is a webcom video
+        vid = sys.argv[2]
+        try:
+            vid = int(vid)
+        except:
+            pass
+
+        # work with video
+        cap = cv2.VideoCapture(vid)
+        # Define the codec and create VideoWriter object
+        size = (
+            int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+            int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        )
+        fourcc = cv2.VideoWriter_fourcc(*'MPEG')  # 'x264' doesn't work
+        out = cv2.VideoWriter('output.avi',fourcc, 20.0, size)
+
         while cap.isOpened():
             # Take each frame
             ret, new_im = cap.read()
 
+            # check if the movie is still going
+            if not ret:
+                break
+
             # compare to last frame if exists
             if old_im is not None:
+                """
                 # Disply two images
                 cv2.imshow("new_im", new_im)
                 cv2.imshow("old_im", old_im)
-
-                # Wait
-                k = cv2.waitKey(0)
-                if k == ESC_KEY:
-                    break
+                """
 
                 # Compute transform
-                print_transform(old_im, new_im)
+                try:
+                    shiftx, shifty = do_transform(old_im, new_im)
+
+                    # add shift
+                    curr_pos[0] += shiftx
+                    curr_pos[1] += shifty
+
+                    # add text to this im
+                    disp_img = new_im.copy()
+                    add_text(disp_img, "X SHIFT: %s (PIXELS)" % curr_pos[0], (20, 120))
+                    add_text(disp_img, "Y SHIFT: %s (PIXELS)" % curr_pos[1], (20, 150))
+                    add_text(disp_img, "Z SHIFT: %s (PIXELS)" % curr_pos[2], (20, 180))
+
+                    # Disply two images
+                    out.write(disp_img)
+
+                except Exception as e:
+                    print e
+                    pass
 
             # Move onto the next frame
             old_im = new_im
 
         cap.release()
+        out.release()
         cv2.destroyAllWindows()
 
 
