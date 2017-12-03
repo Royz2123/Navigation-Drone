d3fe35b045ab87d822361e16f15639dd2ca66e03
Royz2123 <royzohar25@gmail.com>
2017-12-03 16:30:31 +0200

    Works - looks amazing!

--- mvmnt.py
+++ mvmnt.py
@@ -1,23 +1,34 @@
 import cv2
 import sys
+import numpy as np
 
 
 def print_usage():
-    print """
-        Usage: mvmnt.py    [im1]     [im2]
+    print """Invalid arguments:
+
+    Usage: mvmnt.py    [im1]     [im2]
     """
 
 def main():
     if len(sys.argv) < 3:
         print_usage()
-    
+        return
+
     # read the two images
     im1 = cv2.imread(sys.argv[1])
     im2 = cv2.imread(sys.argv[2])
-    
+
+    # normalize
+    im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2HSV)
+    im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2HSV)
+
     # estimate the transform between them
-    transform = cv2.estimateRigidTransform(im1, im2, fullAffine=True)
-    
+    transform = cv2.estimateRigidTransform(
+        im1,
+        im2,
+        fullAffine=True
+    )
+
     print(transform)
 
 
